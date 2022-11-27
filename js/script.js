760ad5b08192e4d80f04cd8c447ba5651b38b406
Last Hope Global <119160109+lasthopeglobal@users.noreply.github.com>
2022-11-27 15:37:45 +0530

    added all the files

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// jquery code
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $('select').formSelect();
+  $('.datepicker').datepicker({
+    autoClose: true,
+    format: "dd/mm/yyyy",
+    setDefaultDate: true,
+    // firstDay: 1,
+  });
+
+  $('input#first_name, input#middle_name,input#last_name,input#mobile_number,textarea#prayerreq ,textarea#address').characterCounter();
+
+  $(".slider").slider({
+    full_width: true,
+    indicators: false,
+    height: 500,
+    interval: 4000
+  });
+  $(".dropdown-trigger").dropdown({ hover: false });
+
+  $('.parallax').parallax();
+  $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+  });
+  autoplay();
+  function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 5000);
+  }
+
+  $('.materialboxed').materialbox();
+
+
+});
+
+// $('.modal').modal();
+// $('.collapsible').collapsible();  
+// $('select').formSelect(); 
+// $('.tabs').tabs();
+// $('.datepicker').datepicker(); 
+// $('.chips').chips();
+
+
+
+// here: Javascript codes 
+
+// function toggleModal() {
+//     var instance = M.Modal.getInstance($("#modal3"));
+//     instance.open();
+// }
+
+
+// Scroll To Top Button With Scroll Progress 
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#3f51b5 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  // conic-gradient(rgb(63, 81, 181) 13%, rgb(215, 215, 215) 13%
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+
+// google translate 
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
+}
---
+++ lhc-project structure.md
@@ -0,0 +1,42 @@
+# last hope chruch project structure 
+/ <br>
+|->  assets<br>
+  |- fonts<br>
+  |- images<br>
+  |- favicon.ico<br>
+|->scripts<br>
+  |- main.js<br>
+  |- main.min.js<br>
+  |- plugins.min.js	<br>
+|-> styles<br>
+  |- bootstrap/materialize.css<br>
+  |- libraires.min.css<br>
+  |- main.css<br>
+- 404 page
+- contact-form page
+- index.html
+ 
+images needed to be created manually 
+- main-site logo
+- favicon
+- [thinking]
+
+
+### light mode colors 
+[1]yellow darken-2
+[2]blue lighten-5
+[3] indigo lighten-2
+
+### dark mode colors
+
+### all over color selection 
+[x] orange accent-3
+[x] yellow darken-3
+[]green darken-1
+[]green darken-2
+[]orange lighten-3
+[]orange lighten-4
+[]#d09e27
+
+https://themes.materializecss.com/pages/docs
+https://themes.materializecss.com/pages/demo
